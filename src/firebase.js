7af9fab522bd9c11f9db74b5bb775880db298936
gowtham <gowthambandlapalli@gmail.com>
2022-08-24 19:03:48 +0530

    latest-commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBd1LE5rF5GGfvkWG-ZNUn5eaDEhWf8y3c",
+    authDomain: "first-project-2be1c.firebaseapp.com",
+    projectId: "first-project-2be1c",
+    storageBucket: "first-project-2be1c.appspot.com",
+    messagingSenderId: "24109744714",
+    appId: "1:24109744714:web:7cabdd5b4d3cd16da65cb3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { initializeApp } from "firebase/app";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBSqKvmTnIHTq0ThAvAkjGMApIbnqJcbB0",
+    authDomain: "first-project-d066b.firebaseapp.com",
+    projectId: "first-project-d066b",
+    storageBucket: "first-project-d066b.appspot.com",
+    messagingSenderId: "848365338448",
+    appId: "1:848365338448:web:0ca5bf58d25dcc6195b655"
+};
 
+const app = initializeApp(firebaseConfig);
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+ReactDOM.render( 
+    < App / > ,
+    document.getElementById('root')
 );
